2a3fc0cf687fd3e65134af895e7cb04cf7a42833
aditya <beingaditya722@gmail.com>
2023-12-19 15:15:55 +0530

    script to find the average

---
+++ average.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+sum=0
+n=0
+while true
+do
+echo "please enter a number(or enter "q" to quit):"
+read var
+if [[ $var =~ ^[0-9]+$ ]]; then
+sum=$(( sum + var ))
+n=$(( n + 1 ))
+elif [[ $var =~ "q" ]]; then
+echo "you opted to quit"
+break
+else 
+echo "invalid input"
+fi
+done
+#calculating the average
+avg=$((sum / n ))
+echo "the average of all the values: $avg"
